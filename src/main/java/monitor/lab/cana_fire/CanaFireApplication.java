b20fdf0b99ba350247cb9ed44d33e8a63f6421bd
Celio <juniorcelioata@gmail.com>
2025-08-11 14:59:02 -0300

    Feat: Insert configuration webClient and model Hotspot

---
+++ src/main/java/monitor/lab/cana_fire/CanaFireApplication.java
@@ -0,0 +1,13 @@
+package monitor.lab.cana_fire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanaFireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanaFireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/monitor/lab/cana_fire/CanaFireApplicationTests.java
@@ -0,0 +1,13 @@
+package monitor.lab.cana_fire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanaFireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
