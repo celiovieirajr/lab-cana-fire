3a8c673fc2cebd641f0511c6d2c0bd86aa2f0d59
Celio <juniorcelioata@gmail.com>
2025-09-20 13:57:04 -0300

    "Tests/Conflicts"

--- src/test/java/monitor/lab/cana_fire/CanaFireApplicationTests.java
+++ src/test/java/monitor/lab/cana_fire/CanaFireApplicationTests.java
@@ -31,21 +31,6 @@ import static org.mockito.Mockito.when;
 class CanaFireApplicationTests {
 
 	@Test
-	void testAlertControllerLastest() {
-		AlertRepository mockRepo = Mockito.mock(AlertRepository.class);
-		AlertController controller = new AlertController(mockRepo);
-
-		Alert alert = new Alert(UUID.randomUUID(), -10.0, -45.0, LocalDateTime.now());
-		when(mockRepo.findTop100ByOrderByDateDesc()).thenReturn(List.of(alert));
-
-		List<Alert> result = controller.latest();
-
-		assertEquals(1, result.size());
-		assertEquals(-10.0, result.get(0).getLat());
-		assertEquals(-45.0, result.get(0).getLon());
-	}
-
-	@Test
 	void testAlertServiceCreateAlert() {
 		AlertRepository mockRepo = Mockito.mock(AlertRepository.class);
 		AlertService service = new AlertService(mockRepo);
@@ -136,7 +121,6 @@ class CanaFireApplicationTests {
 
 	@Test
 	void testUiControllerAddAttribute() {
-		// Arrange
 		AlertRepository mockRepo = mock(AlertRepository.class);
 		UiController controller = new UiController(mockRepo);
 
